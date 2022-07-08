d8fb091780f98e5cd4640d19aaba146415db54db
Matt Luna <mat.c.luna@gmail.com>
2022-07-08 09:33:07 -0500

    feature/cart-ui reducers and tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,7 +1,13 @@
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
+  UPDATE_CURRENT_CATEGORY,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  CLEAR_CART,
+  TOGGLE_CART,
+  REMOVE_FROM_CART
 } from "./actions";
 
 import { useReducer } from 'react';
@@ -24,6 +30,45 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory,
+      };
+    //on these returns, am I defining/creating attributes of the cart?
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      }
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      }
+    //if the action id matches the product id then update, else don't. This allows selective updating of items in the cart without updating all items
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(product => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        })
+      }
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
       };
     
     default:
